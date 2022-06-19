7038ed7a3bff12af0a17142c8bd0dc2a325e46bd
Anthony <gioant0629@gmail.com>
2022-06-19 00:22:36 -0400

    added logic inside function to pass 4 test criterias

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,20 @@
-const repeatString = function() {
+const repeatString = function(string,num) {
 
+    //String variable to store result from for loop
+    let stringResult = '';
+
+    //loop until we reach the number from parameter
+    for(let i = 0; i < num; i++){
+        //for every increment, save the string to variable
+        stringResult += string;
+    }
+
+    //return result after loop ends
+    return stringResult;
 };
+
+//call function to test
+repeatString('hey',3);
 
 // Do not edit below this line
 module.exports = repeatString;
