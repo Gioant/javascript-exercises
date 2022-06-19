6b214ec79dcc9478fe8b5c14d831b41b023b96c0
Anthony <gioant0629@gmail.com>
2022-06-19 00:38:43 -0400

    added string parameter to function and returned 3 methods in a single line. split(), reverse() & join()

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    //combine 3 methods and put them on the same line. Split() to split each letter
+    //Reverse() each letter and finally join() the letters back together
+    return str.split('').reverse().join('');
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
