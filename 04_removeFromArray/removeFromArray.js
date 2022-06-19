8a9d4dce72992f50856664094603a38ec54ba085
Anthony <gioant0629@gmail.com>
2022-06-19 15:08:37 -0400

    function takes 2 parameters, array and optional args. Stores 2nd parameter into an array and used filter() method to return a filtered array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    //store the optional args into an array
+    let newArr = [...args];
 
-};
+    //return the original array but does not include elements from newArr
+    return array.filter(item => !newArr.includes(item));
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
