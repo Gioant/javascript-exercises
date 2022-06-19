8cb60e06b119f70d327b2859afa0f31df316724a
Anthony <gioant0629@gmail.com>
2022-06-19 15:47:01 -0400

    if conditions to check paramters for negativity, strings & non-number parameters. added logic formula to sum a range between 2 nums

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,24 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+
+    //check parameters for negative numbers
+    if(a < 0 || b < 0){
+        return 'ERROR';
+        //if parameters are astring
+    } else if (typeof a == 'string' || typeof b == 'string'){
+        return 'ERROR';
+    }
+
+    //if 1st parameter is greater than 2nd parameter
+    if (a < b){
+    return (b - a + 1) * (a + b) / 2;
+
+    // else do another formula
+    } else if (a > b) {
+        return (a - b + 1) * (b + a) / 2;
+    } else {
+        //else return error
+        return 'ERROR';
+    }
 
 };
 
